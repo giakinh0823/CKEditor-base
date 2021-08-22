b40635e69821e7b37ee9941fe6cacebffafee643
Gia Kinh <giakinh2000@gmail.com>
2021-08-22 19:35:45 +0700

    done

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Editor from 'ckeditor5-custom-build/build/ckeditor';
 import { CKEditor } from '@ckeditor/ckeditor5-react'
+import './style.css'
 
 const editorConfiguration = {
     toolbar: [
@@ -52,6 +53,33 @@ const editorConfiguration = {
         '|',
         'findAndReplace'
     ],
+    heading: {
+        options: [
+            { model: 'paragraph', view: 'p', title: 'Paragraph', class: 'ck-heading_paragraph' },
+            { model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
+            { model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' },
+            { model: 'heading3', view: 'h3', title: 'Heading 3', class: 'ck-heading_heading3' },
+            { model: 'heading4', view: 'h4', title: 'Heading 4', class: 'ck-heading_heading4' },
+            { model: 'heading5', view: 'h5', title: 'Heading 5', class: 'ck-heading_heading5' },
+            { model: 'heading6', view: 'h6', title: 'Heading 6', class: 'ck-heading_heading6' },
+            { model: 'format', view: 'pre', title: 'Formatted', class: 'ck-format' },
+            { model: 'address', view: 'address', title: 'Address', class: 'ck-address' },
+        ],
+    },
+    fontFamily: {
+        options: [
+            'default',
+            'Arial, Helvetica, sans-serif',
+            'Comic Sans MS, cursive',
+            'Courier New, Courier, monospace',
+            'Georgia, serif',
+            'Lucida Sans Unicode, Lucida Grande, sans-serif',
+            'Tahoma, Geneva, sans-serif',
+            'Times New Roman, Times, serif',
+            'Trebuchet MS, Helvetica, sans-serif',
+            'Verdana, Geneva, sans-serif'
+        ]
+    },  
     language: 'vi',
     image: {
         toolbar: [
@@ -71,6 +99,12 @@ const editorConfiguration = {
             'tableProperties'
         ]
     },
+    ckfinder: {
+        options: {
+            resourceType: 'Images'
+        },
+        uploadUrl: "http://127.0.0.1:8000/media/upload/"
+    },
 };
 
 Blog.propTypes = {
@@ -82,29 +116,29 @@ function Blog(props) {
     const [data, setData] = useState("")
 
     return (
-        <div className="App">
+        <div className="">
             <h1>Viết Blog</h1>
             <CKEditor
                 editor={Editor}
                 data=""
                 config={editorConfiguration}
-                onReady={ editor => {
-                    console.log( 'Editor is ready to use!', editor );
+                onReady={editor => {
+                    console.log('Editor is ready to use!', editor);
 
                     // Insert the toolbar before the editable area.
                     editor.ui.getEditableElement().parentElement.insertBefore(
                         editor.ui.view.toolbar.element,
-                        editor.ui.getEditableElement()
+                        editor.ui.getEditableElement(),
                     );
-                } }
-                onError={ ( { willEditorRestart } ) => {
+                }}
+                onError={({ willEditorRestart }) => {
                     // If the editor is restarted, the toolbar element will be created once again.
                     // The `onReady` callback will be called again and the new toolbar will be added.
                     // This is why you need to remove the older toolbar.
-                    if ( willEditorRestart ) {
+                    if (willEditorRestart) {
                         this.editor.ui.view.toolbar.element.remove();
                     }
-                } }
+                }}
                 onChange={(event, editor) => {
                     const data = editor.getData();
                     console.log({ event, editor, data });
